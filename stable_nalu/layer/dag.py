724905247774108919b750d15e5981e549e136e5
marict <paul.michael.curry@gmail.com>
2025-08-08 03:49:25 -0700

    Increase batch size for division operation in DAG layer benchmark script.

--- stable_nalu/layer/dag.py
+++ stable_nalu/layer/dag.py
@@ -16,7 +16,7 @@ python3 experiments/single_layer_benchmark.py --no-cuda --layer-type DAG --opera
 Things to try (on cloud):
 python /Users/paul_curry/ai2/runpod_service/runpod_service.py experiments/single_layer_benchmark.py --layer-type DAG --operation sub --input-size 3 --batch-size 10000 --max-iterations 300000 --log-interval 1000 --clip-grad-norm 1.0 --pod-name nalm-sub
 
-python /Users/paul_curry/ai2/runpod_service/runpod_service.py experiments/single_layer_benchmark.py --layer-type DAG --operation div --input-size 3 --batch-size 10000 --max-iterations 300000 --log-interval 1000 --clip-grad-norm 1.0 --pod-name nalm-div
+python /Users/paul_curry/ai2/runpod_service/runpod_service.py experiments/single_layer_benchmark.py --layer-type DAG --operation div --input-size 3 --batch-size 100000 --max-iterations 300000 --log-interval 1000 --clip-grad-norm 1.0 --pod-name nalm-div
 
 
 """
