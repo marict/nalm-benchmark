bb08124ad5a8f9ee4c9a20cf505c644eced1932a
marict <paul.michael.curry@gmail.com>
2025-08-08 03:45:16 -0700

    Add cloud run examples for sub and div operations in dag.py

--- stable_nalu/layer/dag.py
+++ stable_nalu/layer/dag.py
@@ -13,6 +13,11 @@ python3 experiments/single_layer_benchmark.py --no-cuda --layer-type DAG --opera
 
 python3 experiments/single_layer_benchmark.py --no-cuda --layer-type DAG --operation mul --input-size 3 --batch-size 1000 --max-iterations 300000 --log-interval 1000 --clip-grad-norm 1.0
 
+Things to try (on cloud):
+python /Users/paul_curry/ai2/runpod_service/runpod_service.py experiments/single_layer_benchmark.py --layer-type DAG --operation sub --input-size 3 --batch-size 10000 --max-iterations 300000 --log-interval 1000 --clip-grad-norm 1.0 --pod-name nalm-sub
+
+python /Users/paul_curry/ai2/runpod_service/runpod_service.py experiments/single_layer_benchmark.py --layer-type DAG --operation div --input-size 3 --batch-size 10000 --max-iterations 300000 --log-interval 1000 --clip-grad-norm 1.0 --pod-name nalm-div
+
 
 """
 
