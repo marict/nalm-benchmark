baa188286b2a1aa3443c2eb9b6e9a2c522fb1130
marict <paul.michael.curry@gmail.com>
2025-08-09 13:21:17 -0700

    Update benchmark commands and operations for DAG layer tests.

--- stable_nalu/layer/dag.py
+++ stable_nalu/layer/dag.py
@@ -12,11 +12,11 @@ python3 experiments/single_layer_benchmark.py --no-cuda --layer-type DAG --opera
 
 python3 experiments/single_layer_benchmark.py --no-cuda --layer-type DAG --operation mul --input-size 3 --batch-size 256 --max-iterations 300000 --log-interval 1000 --clip-grad-norm 1.0
 
-python /Users/paul_curry/ai2/runpod_service/runpod_service.py experiments/single_layer_benchmark.py --layer-type DAG --operation mul --input-size 3 --batch-size 256 --max-iterations 300000 --log-interval 100 --clip-grad-norm 1.0 --pod-name nalm-mul
+---
 
-python /Users/paul_curry/ai2/runpod_service/runpod_service.py experiments/single_layer_benchmark.py --layer-type DAG --operation div --input-size 3 --batch-size 256 --max-iterations 10000 --log-interval 100 --clip-grad-norm 1.0 --pod-name nalm-div
+python /Users/paul_curry/ai2/runpod_service/runpod_service.py experiments/single_layer_benchmark.py --layer-type DAG --operation sub --input-size 3 --batch-size 256 --max-iterations 25000 --log-interval 100 --clip-grad-norm 1.0 --pod-name nalm-sub
 
-python /Users/paul_curry/ai2/runpod_service/runpod_service.py experiments/single_layer_benchmark.py --layer-type DAG --operation add --input-size 3 --batch-size 256 --max-iterations 25000 --log-interval 100 --clip-grad-norm 20.0 --pod-name nalm-add-bigbias
+python /Users/paul_curry/ai2/runpod_service/runpod_service.py experiments/single_layer_benchmark.py --layer-type DAG --operation div --input-size 3 --batch-size 256 --max-iterations 25000 --log-interval 100 --clip-grad-norm 1.0 --pod-name nalm-div
 
 
 Things to try (on cloud):
