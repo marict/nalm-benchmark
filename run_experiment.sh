d82fa6cef3b8f80aeeda83b1125309fbe3aa704b
marict <paul.michael.curry@gmail.com>
2025-08-09 05:36:48 -0700

    Make run_experiment.sh executable and add shebang line
    Changed file permissions to executable and added a shebang line for bash.

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 python /Users/paul_curry/ai2/runpod_service/runpod_service.py experiments/single_layer_benchmark.py --layer-type DAG --operation div --input-size 3 --batch-size 256 --max-iterations 25000 --log-interval 100 --clip-grad-norm 1.0 --pod-name nalm-div
 
 python /Users/paul_curry/ai2/runpod_service/runpod_service.py experiments/single_layer_benchmark.py --layer-type DAG --operation mul --input-size 3 --batch-size 256 --max-iterations 25000 --log-interval 100 --clip-grad-norm 1.0 --pod-name nalm-mul
